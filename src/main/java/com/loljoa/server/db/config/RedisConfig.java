1342ba24d63a9e1ad32546c68ace9374398417d1
jeongjin984 <philipwq3963@gmail.com>
2022-04-15 23:36:08 +0900

    m

--- src/main/java/com/loljoa/server/db/config/RedisConfig.java
+++ src/main/java/com/loljoa/server/db/config/RedisConfig.java
@@ -16,45 +16,45 @@ import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 
-@Configuration
-@RequiredArgsConstructor
+//@Configuration
+//@RequiredArgsConstructor
 public class RedisConfig {
 
-    private final ObjectMapper objectMapper;
-
-    @Value("${spring.redis.host}")
-    private String host;
-
-    @Value("${spring.redis.port}")
-    private int port;
-
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
-    }
-
-    @Bean
-    public RedisCacheManager redisCacheManager() {
-        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration
-                .defaultCacheConfig()
-                .disableCachingNullValues()
-                .serializeValuesWith(
-                        RedisSerializationContext.SerializationPair
-                                .fromSerializer(new GenericJackson2JsonRedisSerializer(objectMapper))
-                );
-
-        Map<String, RedisCacheConfiguration> redisCacheConfigurationMap = new HashMap<>();
-        redisCacheConfigurationMap
-                .put("gameData", redisCacheConfiguration.entryTtl(Duration.ofMinutes(5)))
-        ;
-
-        return RedisCacheManager.RedisCacheManagerBuilder
-                .fromConnectionFactory(redisConnectionFactory())
-                .withInitialCacheConfigurations(redisCacheConfigurationMap)
-                .cacheDefaults(redisCacheConfiguration)
-                .build();
-
-    }
+//    private final ObjectMapper objectMapper;
+//
+//    @Value("${spring.redis.host}")
+//    private String host;
+//
+//    @Value("${spring.redis.port}")
+//    private int port;
+//
+//    @Bean
+//    public RedisConnectionFactory redisConnectionFactory() {
+//        return new LettuceConnectionFactory(host, port);
+//    }
+//
+//    @Bean
+//    public RedisCacheManager redisCacheManager() {
+//        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration
+//                .defaultCacheConfig()
+//                .disableCachingNullValues()
+//                .serializeValuesWith(
+//                        RedisSerializationContext.SerializationPair
+//                                .fromSerializer(new GenericJackson2JsonRedisSerializer(objectMapper))
+//                );
+//
+//        Map<String, RedisCacheConfiguration> redisCacheConfigurationMap = new HashMap<>();
+//        redisCacheConfigurationMap
+//                .put("gameData", redisCacheConfiguration.entryTtl(Duration.ofMinutes(5)))
+//        ;
+//
+//        return RedisCacheManager.RedisCacheManagerBuilder
+//                .fromConnectionFactory(redisConnectionFactory())
+//                .withInitialCacheConfigurations(redisCacheConfigurationMap)
+//                .cacheDefaults(redisCacheConfiguration)
+//                .build();
+//
+//    }
 
 
 }
