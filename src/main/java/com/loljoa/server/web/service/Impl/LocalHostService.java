9937aeeff121c0331d3861fadba9647856e7effa
jeongjin984 <philipwq3963@gmail.com>
2022-04-28 15:53:24 +0900

    m

---
+++ src/main/java/com/loljoa/server/web/service/Impl/LocalHostService.java
@@ -0,0 +1,21 @@
+package com.loljoa.server.web.service.Impl;
+
+import org.springframework.stereotype.Service;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Service
+public class LocalHostService {
+
+    public String getLocalHostInfo() {
+        try {
+            InetAddress localhost = InetAddress.getLocalHost();
+            return String.format("host-address: %s host-name: %s",
+                    localhost.getHostAddress(),
+                    localhost.getHostName());
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
