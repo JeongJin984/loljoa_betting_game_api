5b5e4cf1c3cb49e909106f770158c005f8bebea4
jeongjin984 <philipwq3963@gmail.com>
2022-04-29 16:50:02 +0900

    m

--- src/main/java/com/loljoa/server/web/service/Impl/PointDistributeServiceImpl.java
+++ src/main/java/com/loljoa/server/web/service/Impl/PointDistributeServiceImpl.java
@@ -7,19 +7,30 @@ import com.loljoa.server.db.repository.bettingGame.BettingGameRepository;
 import com.loljoa.server.db.repository.bettingState.BettingStateRepository;
 import com.loljoa.server.web.service.PointDistributionService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.LockModeType;
 import javax.transaction.Transactional;
+import java.sql.SQLException;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class PointDistributeServiceImpl implements PointDistributionService {
     private final BettingStateRepository bettingStateRepository;
     private final BettingGameRepository bettingGameRepository;
 
     @Override
-    @Transactional
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Retryable(
+            value = SQLException.class,
+            maxAttempts = 5,
+            backoff = @Backoff(delay = 1000)
+    )
     public void distributePoint(Long gameId, Long choiceId) {
         BettingGame gameData = bettingGameRepository.getGameDataById(gameId);
         for(BettingChoice bc : gameData.getChoices()) {
